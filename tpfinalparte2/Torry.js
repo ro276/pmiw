631c1a758267b421b36d83d14fecfb8cfe65063f
ro276 <rosarri357@gmail.com>
2025-11-15 16:06:55 -0300

    Update Torry.js

--- tpfinalparte2/Torry.js
+++ tpfinalparte2/Torry.js
@@ -11,18 +11,13 @@ class Torry {
     image(this.imgs[0], this.x, this.y, this.sizex, this.sizey);
   }
 
-  mover() {
-    // control de mouse, limitado al area central (150 a 450)
+  mover(){
     this.x = constrain(mouseX, 150, 450 - this.sizex);
     this.y = constrain(mouseY, 0, height - this.sizey);
   }
 
   chocaCon(obj) {
-    return (
-      this.x < obj.x + obj.tam &&
-      this.x + this.sizex > obj.x &&
-      this.y < obj.y + obj.tam &&
-      this.y + this.sizey > obj.y
-    );
+    return (this.x < obj.x + obj.tam && this.x + this.sizex > obj.x && this.y < obj.y + obj.tam && this.y + this.sizey > obj.y);
   }
 }
+
